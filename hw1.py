28af378911950d178975383818c34081c5aab371
Yunki <shinki95@gmail.com>
2017-06-26 15:34:38 +0900

    count 추가

--- hw1.py
+++ hw1.py
@@ -4,13 +4,19 @@ numbers = []
 for x in range(1,21):
     numbers.append(x)
 goal = random.choice(numbers)
+count = 0
 num = 0
+name = input("What is your name: ")
+print(name)
 while num != goal:
     num = int(input())
     if num > goal:
         print("Your guess is too high")
+        count =+ 1
     elif num < goal:
         print("Your guess is too low")
+        count =+ 1
     else:
         print("Good job")
+        print(count)
     
